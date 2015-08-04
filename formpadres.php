212e97aadfead5a480f0a18f08c69c37b2ac6981
Erickerm7 <erick@imagine.com.gt>
2015-08-03 21:29:53 -0500

    Formulario De Padres Prueba

---
+++ formpadres.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fomulario Prueba</title>
+</head>
+
+<body>
+
+<form action="addpadre.php" method="post" name="Padre">
+
+<label>Primer Nombre</label>
+<input name="primerNombre" type="text" size="15" maxlength="50">
+
+<label>Segundo Nombre</label>
+<input name="segundoNombre" type="text" size="15" maxlength="50">
+
+<label>Primer Apellido</label>
+<input name="primerApellido" type="text" size="15" maxlength="50">
+
+<label>Segundo Apellido</label>
+<input name="segundoApellido" type="text" size="15" maxlength="50">
+
+<label>Direccion</label>
+<input name="direccion" type="text" size="15" maxlength="50">
+
+<label>Telefono</label>
+<input name="telefono" type="text" size="15" maxlength="50">
+
+<label>Profesion</label>
+<input name="profesion" type="text" size="15" maxlength="50">
+
+<label>Direccion de Trabajo</label>
+<input name="direccionTrabajo" type="text" size="15" maxlength="50">
+
+<label>Telefono Trabajo</label>
+<input name="telefonoTrabajo" type="text" size="15" maxlength="50">
+
+<label>Correo</label>
+<input name="correoElectronico" type="text" size="15" maxlength="50">
+
+
+<input name="Enviar" type="submit" value="Enviar">
+<input name="Cancelar" type="button" value="Cancelar">
+
+</form>
+
+
+
+</body>
+</html>
